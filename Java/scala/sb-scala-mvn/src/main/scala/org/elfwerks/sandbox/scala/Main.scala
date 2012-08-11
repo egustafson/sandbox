48a8320434bc788a20fe32867f83cf4958b61ec8
Eric Gustafson <eg-github@elfwerks.org>
2012-08-10 21:44:42 -0600

    mvn/scala integrated w/ eclipse

---
+++ Java/scala/sb-scala-mvn/src/main/scala/org/elfwerks/sandbox/scala/Main.scala
@@ -0,0 +1,6 @@
+package org.elfwerks.sandbox.scala
+
+object Main extends App {
+	println("Hello Scala World.")
+	println("done.")
+}
---
+++ Java/scala/sb-scala-mvn/src/test/scala/org/elfwerks/sandbox/scala/DemoTest.scala
@@ -0,0 +1,12 @@
+package org.elfwerks.sandbox.scala
+
+import org.junit._
+import Assert._
+
+@Test
+class DemoTest {
+  
+	@Test
+	def testOk() = assertTrue(true)
+	
+}
