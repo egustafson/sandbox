6726feccb2a82d70dc2c09d5ff50c42bb857d6a2
Eric Gustafson <eg-git@elfwerks.org>
2013-11-05 23:35:10 -0700

    bootstrap java project for dropwizard

---
+++ Java/mvn/dropwizard-basic/src/main/java/org/elfwerks/sandbox/dropwizard/Main.java
@@ -0,0 +1,13 @@
+package org.elfwerks.sandbox.dropwizard;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private final static Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("done.");
+    }
+
+}
---
+++ Java/mvn/dropwizard-basic/src/test/java/org/elfwerks/sandbox/dropwizard/TestMain.java
@@ -0,0 +1,14 @@
+package org.elfwerks.sandbox.dropwizard;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMain {
+
+    @Test
+    public void test() {
+        //fail("Not yet implemented");
+    }
+
+}
