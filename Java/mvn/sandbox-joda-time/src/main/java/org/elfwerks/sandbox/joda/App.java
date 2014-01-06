328b700a9c3319908e85d4208a7617096072e30d
Eric Gustafson <eg-github@elfwerks.org>
2014-01-05 20:40:42 -0700

    filled in very basic app for joda time example, existing test cases had most of the examples

--- Java/mvn/sandbox-joda-time/src/main/java/org/elfwerks/sandbox/joda/App.java
+++ Java/mvn/sandbox-joda-time/src/main/java/org/elfwerks/sandbox/joda/App.java
@@ -1,13 +1,18 @@
 package org.elfwerks.sandbox.joda;
 
-/**
- * Hello world!
- *
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
+
+/* See unit tests for additional use-cases
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+    
+    public static void main( String[] args ) {
+        DateTimeFormatter isoFmt = ISODateTimeFormat.dateTime();
+        DateTime now = new DateTime();
+        System.out.println("It is now "+isoFmt.print(now));
     }
+    
 }
