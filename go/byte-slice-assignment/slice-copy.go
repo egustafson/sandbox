49995f97413c0afe32da81b221ce8386c6926930
Eric Gustafson <eg-git@elfwerks.org>
2017-03-17 14:51:01 -0600

    go/slices - slice copy, including through chan

--- go/byte-slice-assignment/slice-copy.go
+++ go/byte-slice-assignment/slice-copy.go
@@ -17,15 +17,28 @@ func main() {
 	// and print both
 	fmt.Printf("s1: %v\n", s1)
 	fmt.Printf("s2: %v\n", s2)
-	//
+
 	// what not to do
 	//
 	s3 := []byte{1, 2, 3, 4, 5, 6}
-	s4 := s3 // <-- 2nd reference to the same underlying array
+	s4 := s3 // <-- FAIL:  2nd reference to the same underlying array
 	for c, _ := range s4 {
 		s3[c] = 0
 	}
 	println("-----------")
 	fmt.Printf("s3: %v\n", s3)
 	fmt.Printf("s4: %v\n", s4)
+
+	// slice through channel -- also colbered
+	//
+	s5 := []byte{1, 2, 3, 4, 5, 6}
+	ch := make(chan []byte, 1)
+	ch <- s5 // <-- FAIL:  passes reference to the same underlying array
+	s6 := <-ch
+	for c, _ := range s6 {
+		s5[c] = 0
+	}
+	println("-----------")
+	fmt.Printf("s5: %v\n", s5)
+	fmt.Printf("s6: %v\n", s6)
 }
