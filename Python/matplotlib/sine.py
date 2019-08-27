551fa1f8ffd52331f084e91ccbdeed494efa3be2
Eric Gustafson <eg-git@elfwerks.org>
2019-08-26 22:49:19 -0600

    Python: matplotlib

---
+++ Python/matplotlib/sine.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+t = np.arange(0.0, 2.0, 0.01)
+s = 1 + np.sin(2 * np.pi * t)
+
+fig, ax = plt.subplots()
+ax.plot(t,s)
+
+ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+       title='About as simple as it gets, folks')
+ax.grid()
+
+#fig.savefig("test.png")
+print("Using backend: {}".format(matplotlib.get_backend()))
+plt.show()
