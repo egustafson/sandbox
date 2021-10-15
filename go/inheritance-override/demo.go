56fc1c82c75afc68b9b0c1ddda036f879a5df415
Eric Gustafson <eg-git@elfwerks.org>
2021-10-15 10:39:49 -0600

    go: expand method override, show base is invoked when struct is embedded

--- go/inheritance-override/demo.go
+++ go/inheritance-override/demo.go
@@ -12,6 +12,11 @@ type Super struct {
 	Base
 }
 
+type Super2 struct {
+	Base
+	Ver int
+}
+
 func (b *Base) Operation() {
 	fmt.Printf("Base.Operation() on %s\n", b.Name)
 }
@@ -32,11 +37,18 @@ func main() {
 	s := Super{
 		Base: Base{Name: "Super-1"},
 	}
+	s2 := Super2{
+		Base: Base{Name: "Super-2"},
+		Ver:  2,
+	}
 
 	fmt.Println("----")
 	b.Operation()
 	fmt.Println("")
 	s.Operation()
+	fmt.Println("")
+	fmt.Println("expect Base.Operation() to be invoked")
+	s2.Operation()
 	fmt.Println("")
 
 	fmt.Println("done.")
