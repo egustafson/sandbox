2526eaed99337590f0cacd8b0ba45f3f48f9c68d
Eric Gustafson <eg-git@elfwerks.org>
2021-12-15 16:25:24 -0700

    go: expanded grpc streaming example with detection for stream cancelation / disconnect

--- go/grpc-streaming/client.go
+++ go/grpc-streaming/client.go
@@ -9,6 +9,8 @@ import (
 	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"github.com/egustafson/sandbox/go/grpc-streaming/pb"
 )
@@ -23,37 +25,57 @@ func main() {
 
 	client := pb.NewDemoServiceClient(conn)
 	doConsume(client)
-	//
-	// Now, sleep for a while in an attempt to timeout the server's socket
-	//
-	time.Sleep(120 * time.Second)
+	time.Sleep(1 * time.Second) // give the gRPC library a moment to clean up.
 	log.Print("done.")
 }
 
 func doConsume(client pb.DemoServiceClient) {
 
+	reqId := os.Getpid()
 	hreq := &pb.HeartbeatRequest{
-		RequestId: fmt.Sprintf("%d", os.Getpid()),
+		RequestId: fmt.Sprintf("%d", reqId),
 	}
+	log.Printf("requestId: %d", reqId)
 
-	stream, err := client.ListenHeartbeat(context.Background(), hreq)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	stream, err := client.ListenHeartbeat(ctx, hreq)
 	if err != nil {
 		log.Fatalf("client call failed: %s", err)
 	}
 	ii := 0
 	for {
 		hb, err := stream.Recv()
-		if err == io.EOF {
-			break
-		}
 		if err != nil {
-			log.Fatalf("%v.ListFeatures(_) = _, %v", client, err)
+			//
+			// remote (Server) closed the stream (of its own accord).
+			//
+			if err == io.EOF {
+				log.Print("received io.EOF, stream ended (properly)")
+				return
+			}
+			//
+			// (this) client canceled the stream
+			//   all (local) messages have been consumed
+			//
+			if status.Code(err) == codes.Canceled {
+				log.Print("received grpc Canceled")
+				return
+			}
+			//
+			// Unexpected error
+			//
+			log.Fatalf("stream returned error: %v.ListFeatures(_) = _, %v", client, err)
 		}
 		log.Println(hb.Note)
 		ii++
 		if ii > 10 {
-			log.Print("reached limit: returning from doConsume()")
-			return
+			log.Print("reached limit: canceling context")
+			//
+			// Cancel channel context (orderly shutdown)
+			//  keep consuming, there may be messages in-flight.
+			//
+			cancel()
 		}
 	}
 }
--- go/grpc-streaming/server.go
+++ go/grpc-streaming/server.go
@@ -7,6 +7,8 @@ import (
 	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"github.com/egustafson/sandbox/go/grpc-streaming/pb"
 )
@@ -24,6 +26,7 @@ func main() {
 	grpcServer := grpc.NewServer()
 	pb.RegisterDemoServiceServer(grpcServer, &s)
 
+	log.Print("Server running...")
 	if err := grpcServer.Serve(l); err != nil {
 		log.Fatalf("failed to serve: %s", err)
 	}
@@ -37,12 +40,29 @@ func (s *DemoServer) ListenHeartbeat(req *pb.HeartbeatRequest, stream pb.DemoSer
 			Note: fmt.Sprintf("beat: %d", ii),
 		}
 		if err := stream.Send(h); err != nil {
-			log.Printf("stream ending: %v", err)
+			//
+			// Stream was canceled by the client (orderly teardown)
+			//
+			if status.Code(err) == codes.Canceled {
+				log.Printf("stream(%s) Canceled, stream terminating", req.RequestId)
+				return nil
+			}
+			//
+			// Connection to client abrubtly closed (unorderly teardown)
+			//
+			if status.Code(err) == codes.Unavailable {
+				log.Printf("stream(%s) Unavailable (transport closing), stream terminating", req.RequestId)
+				return nil
+			}
+			//
+			// Unexpected error
+			//
+			log.Printf("stream(%s) error: %v", req.RequestId, err)
 			return err
 		}
 		ii = ii + 1
 		log.Println("tick.")
-		time.Sleep(time.Second)
+		time.Sleep(250 * time.Millisecond)
 	}
 	return nil
 }
