bec45fcbc3a89868cd9162d74fb94015fac6f844
Eric Gustafson <ericg@elfwerks.io>
2024-08-07 22:13:55 -0600

    learning rust: rework minigrep with closures

--- rust/rust-book/ch13-closures/redo-ch12-minigrep/src/lib.rs
+++ rust/rust-book/ch13-closures/redo-ch12-minigrep/src/lib.rs
@@ -8,13 +8,19 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(
+        mut args: impl Iterator<Item = String>
+    ) -> Result<Config, &'static str> {
+        args.next(); // throw away the program exec path
+        
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("query string missing")
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("file path missing")
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
     
@@ -43,14 +49,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(
@@ -58,14 +60,10 @@ pub fn search_case_insensitive<'a>(
     contents: &'a str,
 ) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line  in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
--- rust/rust-book/ch13-closures/redo-ch12-minigrep/src/main.rs
+++ rust/rust-book/ch13-closures/redo-ch12-minigrep/src/main.rs
@@ -3,15 +3,10 @@ use std::{env, process};
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-
-    //println!("searching for pattern: '{}'", config.query);
-    //println!("in file: {}", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
         eprintln!("application error: {e}");
