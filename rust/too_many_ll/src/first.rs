7263c7d221b82eb814c1b719c68f006b9acb581a
Eric Gustafson <ericg@elfwerks.io>
2024-09-16 14:48:11 -0600

    rust: learning - 'too many linked lists', cont.

--- rust/too_many_ll/src/first.rs
+++ rust/too_many_ll/src/first.rs
@@ -38,6 +38,15 @@ impl List {
     }
 }
 
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::List;
